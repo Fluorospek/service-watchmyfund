1278df27c73e22a4194c08ae60fd5221c60490d5
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2026-09-06 01:31:06 +0530

    intial commit

---
+++ src/main/java/com/tanmay/watchmyfund/WatchmyfundApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay.watchmyfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchmyfundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchmyfundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmay/watchmyfund/WatchmyfundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmay.watchmyfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchmyfundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
